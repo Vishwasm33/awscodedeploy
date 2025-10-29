14ae89ed4d7f0709263c80c2922ef9ff42dee874
VISHWAS M <121951898+Vishwasm33@users.noreply.github.com>
2025-10-29 11:33:59 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -4,13 +4,13 @@ set -e
 echo "Stopping running Docker container (if any)..."
 
 # Get container ID of running app
-containerid=$(docker ps -q --filter "name=samplepython")
+containerid=$(sudo docker ps -q --filter "name=samplepython")
 
 # Stop and remove container if found
 if [ -n "$containerid" ]; then
   echo "Stopping container ID: $containerid"
-  docker stop "$containerid"
-  docker rm "$containerid"
+  sudo docker stop "$containerid"
+  sudo docker rm "$containerid"
   echo "Container stopped and removed successfully."
 else
   echo "No running container found."
