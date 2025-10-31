d0ebec5eb4003911bd9361ef7b406f0cd6043cc2
VISHWAS M <121951898+Vishwasm33@users.noreply.github.com>
2025-11-01 00:37:32 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 set -e
+export PATH=$PATH:/usr/bin
 
 echo "Starting Docker container..."
 
 # Pull latest image
-sudo docker pull vishwasm0881/simple-python-flask-app:latest
+/usr/bin/docker pull vishwasm0881/simple-python-flask-app:latest
 
 # Stop old container if running
-containerid=$(sudo docker ps -q --filter "name=samplepython")
+containerid=$(/usr/bin/docker ps -q --filter "name=samplepython")
 if [ -n "$containerid" ]; then
   echo "Stopping old container ID: $containerid"
-  sudo docker stop "$containerid"
-  sudo docker rm "$containerid"
+  /usr/bin/docker stop "$containerid"
+  /usr/bin/docker rm "$containerid"
 fi
 
 # Start new container
-sudo docker run -d -p 5000:5000 --name samplepython vishwasm0881/simple-python-flask-app:latest
+/usr/bin/docker run -d -p 5000:5000 --name samplepython vishwasm0881/simple-python-flask-app:latest
 
 echo "Container started successfully!"
-
-
-
