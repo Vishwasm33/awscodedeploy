dc32b66abbf8a089304404fb762dac3780140fca
VISHWAS M <121951898+Vishwasm33@users.noreply.github.com>
2025-11-01 01:30:53 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,5 +5,5 @@ set -e
 docker pull vishwasm0881/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 vishwasm0881/simple-python-flask-app
+docker run -d -p 8000:8000 vishwasm0881/simple-python-flask-app
 
